1721c30e3f34890d0469606bffdc657ba4b71a7d
emmanuel tindi <emmanueltindi66@gmail.com>
2024-09-01 09:11:52 +0300

    my portfolio

---
+++ my-portfolio/src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+import Body from './Components/Body'
+import Footer from './Components/Footer/Footer'
+import Header from './Components/Header/Header'
+
+function App() {
+
+
+  return (
+    <>
+      <Header />
+      <Body />
+      <Footer />
+    </>
+  )
+}
+
+export default App
---
+++ my-portfolio/src/Components/Body/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+function Body() {
+    return (
+        <div>
+            This is the body
+        </div>
+    )
+}
+
+export default Body;
